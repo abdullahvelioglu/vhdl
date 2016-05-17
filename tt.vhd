0cc1d4c75722074e8dc3300b4b53e86aac986cb2
Abdullah Velioglu <abdullahvelioglu08@gmail.com>
2016-05-18 00:21:49 +0300

    Add Full adder circuit block

--- tt.vhd
+++ tt.vhd
@@ -85,3 +85,33 @@ begin
         or_o <= or_i1 or or_i2;
     end process;
 end behv;
+
+-------------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity fa is
+    port( fa_i1:     in STD_LOGIC;
+          fa_i2:     in STD_LOGIC;
+          fa_icarry: in STD_LOGIC;
+          fa_total:  out STD_LOGIC;
+          fa_ocarry: out STD_LOGIC);
+end fa;
+
+architecture behv of fa is
+    component ha is
+      port( ha_i1: in STD_LOGIC;
+            ha_i2: in STD_LOGIC;
+            ha_total: out STD_LOGIC;
+            ha_carry: out STD_LOGIC);
+    end component;
+
+    signal secondary_cable1: STD_LOGIC;
+    signal secondary_cable2: STD_LOGIC;
+    signal secondary_cable3: STD_LOGIC;
+begin
+    block1: ha  port map(ha_i1 => fa_i1,ha_i2 => fa_i2, ha_carry => secondary_cable1, ha_total => secondary_cable2);
+    block2: ha  port map(ha_i1 => secondary_cable2, ha_i2 => fa_icarry, ha_carry => secondary_cable3 ha_total => fa_total);
+    block3: OR_GATE port map(or_i1 => secondary_cable1, or_i2 => secondary_Cable3, or_o => fa_ocarry);
+end behv;
