d12357622272c2e4d6a213d5ec79c57fcea09fbd
Abdullah Velioglu <abdullahvelioglu08@gmail.com>
2016-05-18 00:11:00 +0300

    Add Or Gate

--- tt.vhd
+++ tt.vhd
@@ -65,3 +65,23 @@ begin
     block1: XOR_GATE port map(xor_i1 => ha_i1,xor_i2 => ha_i2,xor_o => ha_total);
     block2: AND_GATE port map(and_i1 => ha_i1,and_i2 => ha_i2, and_o => ha_carry);
 end behv;
+
+-----------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity OR_GATE is
+    port( or_i1: in STD_LOGIC;
+          or_i2: in STD_LOGIC;
+          or_o:  in STD_LOGIC);
+end OR_GATE;
+
+architecture behv of OR_GATE is
+
+begin
+    process(or_i1,or_i2)
+    begin
+        or_o <= or_i1 or or_i2;
+    end process;
+end behv;
